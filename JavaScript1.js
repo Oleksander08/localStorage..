e073d6263260c6910342369f8999f84fdbdb9688
Oleksander08 <look98727@gmail.com>
2025-06-23 15:01:58 +0300

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,17 @@
+﻿const reguest = (endpoint) => {
+    fetch(endpoint)
+        .then((result) => {
+            return result.json();
+        })
+        .then((result) => {
+            result.results.forEach(character => {
+                console.log(`${character.name}, Species: ${character.species}`);
+            });
+        });
+};
+
+reguest('https://rickandmortyapi.com/api/character/?page=1')
+reguest('https://rickandmortyapi.com/api/character/?page=10')
+reguest('https://rickandmortyapi.com/api/character/?page=4')
+reguest('https://rickandmortyapi.com/api/character/?page=6')
+reguest('https://rickandmortyapi.com/api/character/?page=12')
